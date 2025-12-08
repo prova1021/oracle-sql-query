9ad2efba1620b4e3a40df318684f133d3f866446
prova1021 <anikatahsindev@gmail.com>
2025-12-08 11:17:27 +0600

    Update lab5.sql

--- lab5.sql
+++ lab5.sql
@@ -1,2 +1,4 @@
 
-select * from customeres where phone = '01710656237' and like 'S%'
+select * from customers where phone = '01710656237' and like 'S%'
+
+select * from customers cs inner join orders o on cs.id = o.customer_id where phone = '01710656237'
